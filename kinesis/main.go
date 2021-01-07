f3de20a8be94d71e4c3fd85506ee40aa11a6d741
synapticcleft <39135798+pafable@users.noreply.github.com>
2021-01-06 20:32:36 -0500

    Update main.go

--- kinesis/main.go
+++ kinesis/main.go
@@ -14,7 +14,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
